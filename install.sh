4cc7059ecedaf7689d3c7d90de3c9e4164dee44c
rokdd <rokdd@web.de>
2021-12-07 20:09:05 +0100

    Initial

--- install.sh
+++ install.sh
@@ -6,19 +6,17 @@
 # sudo curl https://raw.githubusercontent.com/rokdd/green-venv/master/install.sh | sudo bash
 
 # Terminal colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-GRAY='\033[0;30m'
 
-NC='\033[0m' # No Color
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
 # Prints a line with color using terminal codes
 style_print() {
-  printf "%s\n" "${2}$1${NC}"
+  echo "${2}$1${reset}"
 }
 
 style_print "test for normal"
-style_print "test for green" GREEN
+style_print "test for green" 'green'
 
 grep -qxF '[ -f $HOME/.bashrc.greenenv ] && . $HOME/.bashrc.greenenv' $HOME/.bashrc || echo '[ -f $HOME/.bashrc.greenenv ] && . $HOME/.bashrc.greenenv' >> $HOME/.bashrc
